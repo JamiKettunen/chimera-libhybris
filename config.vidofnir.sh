80edb1a2c042ad1a872b6f344da981de2b34f476
Jami Kettunen <jami.kettunen@protonmail.com>
2024-10-04 01:20:56 +0300

    config.vidofnir.sh: Link to kernel source

--- config.vidofnir.sh
+++ config.vidofnir.sh
@@ -1,4 +1,5 @@
 # mkrootfs.sh config for Volla Phone X23 (volla-vidofnir)
+# v5.10 kernel source: https://gitlab.com/ubports/porting/reference-device-ports/halium12/volla-x23/kernel-volla-mt6789
 OVERLAYS+=(
 	halium-12 # VNDK 31/32 adaptations
 	mtk-extras # MediaTek SoC

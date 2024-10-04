06df9450f9550fcb2da437ca76b01fc61ec2fe27
Jami Kettunen <jami.kettunen@protonmail.com>
2024-10-05 02:02:43 +0300

    overlays/base: Fix deployed packages regression
    Previously we used to install everything always minus apks that were
    explicitly deleted, but now nothing explicitly depends on
    libgles1-hybris (but we still probably want it around?) and we also use
    libhybris-test-progs in chimera-libhybris-tests at least; it implicitly
    pulls in lib{egl,gles2,opencl}-hybris but let's get them explicitly too

--- overlays/base/deploy.sh
+++ overlays/base/deploy.sh
@@ -1,2 +1,5 @@
 #!/bin/sh -ex
-apk add wayfire-droidian@hybris-cports libgbinder-progs@hybris-cports dinit@hybris-cports
+apk add wayfire-droidian@hybris-cports libgbinder-progs@hybris-cports dinit@hybris-cports \
+    libhybris-test-progs@hybris-cports libegl-hybris@hybris-cports libgles2-hybris@hybris-cports \
+    libopencl-hybris@hybris-cports \
+    libgles1-hybris@hybris-cports

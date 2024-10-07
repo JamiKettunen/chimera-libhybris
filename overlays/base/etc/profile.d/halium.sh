77e318e88156d15e45bcc503d7e76d8102c11a60
Jami Kettunen <jami.kettunen@protonmail.com>
2024-10-07 22:37:14 +0300

    base: halium.sh: Add a still booting notice

--- overlays/base/etc/profile.d/halium.sh
+++ overlays/base/etc/profile.d/halium.sh
@@ -7,11 +7,17 @@ case $- in
   *) return ;;
 esac
 
-# prompt about running initial tests on first successful boot (not dinit-panic) and when not yet asked
-if [ -f /run/dinit/first-boot ] && [ ! -f /tmp/.chimera_no_libhybris_tests ] && [ ! -f /run/dinit/failed-boot ]; then
-    touch /tmp/.chimera_no_libhybris_tests
-    read -p 'Run quick initial boot libhybris/Halium Android container tests (Y/n)? ' ans
-    case "${ans^^}" in 'Y'*|'') doas chimera-libhybris-tests ;; esac
+if [ ! -f /run/dinit/failed-boot ]; then
+  # give at least a clear hint when the Halium container boot process could have problems
+  if [[ "$(dinitctl status android.target)" != *"STARTED"* ]]; then
+    echo -e "\e[1;33mNOTICE: The system is still booting (android.target not reached)\e[0m\n"
+  fi
+  # prompt about running initial tests on first successful boot (not dinit-panic) and when not yet asked
+  if [ -f /run/dinit/first-boot ] && [ ! -f /tmp/.chimera_no_libhybris_tests ]; then
+      touch /tmp/.chimera_no_libhybris_tests
+      read -p 'Run quick initial boot libhybris/Halium Android container tests (Y/n)? ' ans
+      case "${ans^^}" in 'Y'*|'') doas chimera-libhybris-tests ;; esac
+  fi
 fi
 
 echo -e "Welcome to \e[35m\e]8;;https://chimera-linux.org\aChimera Linux\e]8;;\a\e[0m (with \e[32m\e]8;;https://github.com/libhybris/libhybris\alibhybris\e]8;;\a\e[0m) on kernel \e[1;33m$(uname -r)\e[0m ($(uptime -p))! ^^"

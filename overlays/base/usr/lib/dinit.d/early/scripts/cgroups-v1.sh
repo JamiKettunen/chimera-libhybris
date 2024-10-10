c96af9d41e7b26c4ee4eecca990868faa087b526
Jami Kettunen <jami.kettunen@protonmail.com>
2024-10-10 15:01:28 +0300

    base: cgroups-v1.sh: Drop any 100% likely to fail v2 setup stuff
    This is solely meant as a fallback for old kernels and will fail to
    setup v2 cgroups every single time if tried.

--- overlays/base/usr/lib/dinit.d/early/scripts/cgroups-v1.sh
+++ overlays/base/usr/lib/dinit.d/early/scripts/cgroups-v1.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+# adapted https://github.com/chimera-linux/dinit-chimera/commit/c43985d removals using
+# ./early/helpers/mntpt and without attempting non-functional v2 stuff in this fallback
 set -e
 
 # cgroup mounts
 _cgroupv1="/sys/fs/cgroup"
-_cgroupv2="${_cgroupv1}/unified"
 
 # cgroup v1
 ./early/helpers/mntpt "$_cgroupv1" || mount -o mode=0755 -t tmpfs cgroup "$_cgroupv1"
@@ -13,9 +14,3 @@ while read -r _subsys_name _hierarchy _num_cgroups _enabled; do
     mkdir -p "$_controller"
     ./early/helpers/mntpt "$_controller" || mount -t cgroup -o "$_subsys_name" cgroup "$_controller"
 done < /proc/cgroups
-
-# cgroup v2
-mkdir -p "$_cgroupv2"
-./early/helpers/mntpt "$_cgroupv2" || mount -t cgroup2 -o nsdelegate cgroup2 "$_cgroupv2" || \
-  mount -t cgroup2 cgroup2 "$_cgroupv2" || \
-  rmdir "$_cgroupv2"

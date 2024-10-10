d278b793a379783daf1939261ca06e5731b51615
Jami Kettunen <jami.kettunen@protonmail.com>
2024-10-10 22:51:41 +0300

    meta: Move installing unudhcpd into overlays/usbnet
    USB networking setup was the reason we were installing it in mkrootfs.sh
    and it belongs in the overlay along with everything else.

--- mkrootfs.sh
+++ mkrootfs.sh
@@ -159,7 +159,7 @@ apk add -t .base-minimal-custom-hybris base-full \
   !base-full-net openssh \
   !base-full-sound
 apk add \
-  bash unudhcpd htop fastfetch neovim psmisc tree networkmanager \
+  bash htop fastfetch neovim psmisc tree networkmanager \
   ncdu ripgrep strace llvm-binutils erofs-utils lsof vulkan-tools mesa-utils conspy bluez libinput evtest upower \
   greetd xwayland hicolor-icon-theme fonts-cantarell-otf gnome-console gsettings-desktop-schemas wtype wlr-randr wayland-utils
 
---
+++ overlays/usbnet/deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/sh -ex
+apk add unudhcpd

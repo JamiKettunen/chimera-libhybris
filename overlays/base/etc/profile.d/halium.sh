74b35214877eb7752f6537959248dd887ca6d4a8
Jami Kettunen <jami.kettunen@protonmail.com>
2024-10-21 18:04:29 +0300

    base: Better path for notice disabling marker file
    Move it from /.chimera_hide_libhybris_notice to
    /etc/default/hide-libhybris-notice.

--- overlays/base/etc/profile.d/halium.sh
+++ overlays/base/etc/profile.d/halium.sh
@@ -22,7 +22,7 @@ fi
 
 echo -e "Welcome to \e[35m\e]8;;https://chimera-linux.org\aChimera Linux\e]8;;\a\e[0m (with \e[32m\e]8;;https://github.com/libhybris/libhybris\alibhybris\e]8;;\a\e[0m) on kernel \e[1;33m$(uname -r)\e[0m ($(uptime -p))! ^^"
 
-if [ ! -f /.chimera_hide_libhybris_notice ]; then
+if [ ! -f /etc/default/hide-libhybris-notice ]; then
   echo -e "
 \e[31mPlease \e[1mDO NOT\e[0m\e[31m report any issues to upstream Chimera Linux, they're not
 responsible for anything in particular until confirmed it's for sure not
@@ -33,5 +33,5 @@ and https://github.com/JamiKettunen/chimera-libhybris
 
 To conduct some tests for an initial port run \e[1mdoas chimera-libhybris-tests\e[0m
 
-Once you understand this you may hide most of this with \e[1mdoas touch /.chimera_hide_libhybris_notice\e[0m"
+Once you understand this you may hide most of this with \e[1mdoas touch /etc/default/hide-libhybris-notice\e[0m"
 fi

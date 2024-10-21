42f97ddfa1eb34d3250995fcbe4e88477ff294fb
Jami Kettunen <jami.kettunen@protonmail.com>
2024-10-21 14:12:27 +0300

    base+DEBUGGING: Support rootfs mounted as read-only by default
    This way we can drop both the ugly root-remount hack for image files as
    well as get working fsck!
    
    Also switch default tested Halium initrd setup over to this by the means
    of dropping /.writable_image

--- overlays/base/deploy.sh
+++ overlays/base/deploy.sh
@@ -4,14 +4,15 @@ apk add lxc-android@hybris-cports nyagetty@hybris-cports \
     libgles1-hybris@hybris-cports \
     bluebinder@hybris-cports
 
-# let's make a relative /data symlink instead of absolute one by default coming from Halium initrd :^)
-ln -sr /android/data /data
-
-# get a read-write rootfs by default when using Halium initrd (without having to create this in userdata root otherwise)
-touch /.writable_image
-
 # we only care about tty1 (if even that) for conspy -> GUI launch; used to apk add !base-full-console dmesg
 [ -f /etc/default/console-setup ] && sed -i '' 's:ACTIVE_CONSOLES=.*:ACTIVE_CONSOLES="/dev/tty1":' /etc/default/console-setup
+
+# create /userdata Halium initrd would normally make with rw rootfs (we don't touch /.writable_image
+# to fix both early-root-{remount,fsck} for loopback images at least)
+mkdir /userdata
+
+# let's make a relative /data symlink instead of absolute one by default coming from Halium initrd :^)
+ln -sr /android/data /data
 
 # HACK: init wrapper to get verbose dinit logs in rootfs /dinit.log by default (typically no functional VT framebuffer)
 ln -sf preinit /usr/bin/init # originally pointing to dinit

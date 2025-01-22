9dcce95e51f893845db9f4ecdba2d16cad23127b
Jami Kettunen <jami.kettunen@protonmail.com>
2025-01-22 12:46:00 +0200

    base: Adapt cgroup hacks to dinit-chimera v0.99.16+
    mntpt was renamed to mnt, start using absolute paths etc.

--- overlays/base/usr/lib/dinit.d/early/scripts/cgroups-v1.sh
+++ overlays/base/usr/lib/dinit.d/early/scripts/cgroups-v1.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 # adapted https://github.com/chimera-linux/dinit-chimera/commit/c43985d removals using
-# ./early/helpers/mntpt and without attempting non-functional v2 stuff in this fallback
+# /usr/lib/dinit.d/early/helpers/mnt and without attempting non-functional v2 stuff in this fallback
 set -e
 
 # cgroup mounts
 _cgroupv1="/sys/fs/cgroup"
 
 # cgroup v1
-./early/helpers/mntpt "$_cgroupv1" || mount -o mode=0755 -t tmpfs cgroup "$_cgroupv1"
+/usr/lib/dinit.d/early/helpers/mnt try "$_cgroupv1" cgroup tmpfs "mode=0755"
 while read -r _subsys_name _hierarchy _num_cgroups _enabled; do
     [ "$_enabled" = "1" ] || continue
     _controller="${_cgroupv1}/${_subsys_name}"
     mkdir -p "$_controller"
-    ./early/helpers/mntpt "$_controller" || mount -t cgroup -o "$_subsys_name" cgroup "$_controller"
+    /usr/lib/dinit.d/early/helpers/mnt try "$_controller" cgroup cgroup "$_subsys_name"
 done < /proc/cgroups
